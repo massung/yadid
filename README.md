67ea9180a41bef97dec67de2b43259ff7ae3b455
Jeffrey Massung <massung@gmail.com>
2019-09-26 14:50:29 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,94 @@
+# Yadid
+
+Yadid is a [Visual Studio Code][vscode] extension for the [8th][8th] programming langauge. Yadid ("friend" in Hebrew) gets its name from the fact that 8th's creator - [Ron Aaron][ron] - lives in Israel, and I like to think of this extension as a programmer's friend.
+
+## Features
+
+Forth - and its derivatives - are actually quite frustrating to make an effective extension and syntax highlighter for. This is because they have little-to-no syntax and are very context dependent. For this reason, Yadid's primary goals are to provide:
+
+* Context-aware syntax highlighting
+* Quick running the current file as an 8th script
+* Building the project in the current workspace
+
+### Context-Aware Syntax Highlighting
+
+Yadid's syntax highlighting has no intention - nor will it ever - of highlighting every known word in the dictionary. I personally believe this is not only difficult to maintain, but an eye sore and not effective. It would like a C syntax highlighter only calling attention to the libc functions and nothing else. Of what use is that to anyone? Instead, Yadid highlights words that fit one of the following criteria:
+
+* Defining words (e.g. `:`, `var`, `ns:`, `constant`)
+* State-changing words (e.g. `(`, `;`)
+* Immediate words (e.g. `'`, `p:`)
+* Control-flow words (e.g. `if`, `while`, `again`)
+* Exiting words (e.g. `null;`, `;if`)
+
+In addition, because Yadid is picky about this, it can go further and only highlight those words _in the state(s) they are legally allowed to be used in!_ This is where Yadid shines. For example, consider the following animation of code being edited and notice how the highlighting changes based on the state:
+
+![context-aware-highlighting](images/screencast.gif)
+
+Notice how when defining the word `run-test` that `: run-tests` is no longer highlighted until after the definition of `run-test` is ended and back in interpret mode. And, when replacing the anonymous word `( test drop )` with `' run-test`, the closing `)` becomes unhighlighted once the opening `(` is deleted.
+
+There are many of these situations for which I hope Yadid will be very helpful. I've found this especially true with respect to literal arrays and maps. For example, literal characters are not allowed in literal arrays (this has bit me multiple times) and so won't be highlighted there.
+
+### Running 8th Scripts
+
+The currently open file can be run as an 8th script with the "Run File" command (`F6`) or the "Run File with Arguments" (`Ctrl-F6`).
+
+### Project Building
+
+If open in an Workspace and there is a 8th build project setup in the folder, then the "Build Project" (`Ctrl-Shift-B`) command can be used to build it. Likewise, the "Launch Build GUI" command can be used to start the 8th build GUI to create a project, change its configuration, or build it.
+
+## Requirements
+
+This extension assumes you have `8th` installed, that it exists in your `PATH` somewhere, and (to build projects) that the `build` binary is up one directory from the `8th` executable as it would be in a standard installation.
+
+## Known Issues and Quirks
+
+In order to guarantee that what's highlighted is correct, there are a couple areas where what _isn't_ highlighted is valid 8th, but doesn't show that way:
+
+Literal arrays and map items not padding the `,` delimiter:
+
+```8th
+[true,false]  \ valid 8th, but won't highlight correctly
+```
+
+Defining words on the different lines:
+
+```8th
+:
+foo  \ valid 8th, but won't highlight correctly
+;
+```
+
+Regular expressions are distinguished from `n:/` by assuming that any `/` paired with another `/` _with no whitespace between them_ is a regular expression. Of course, this means that a regular expression with whitespace in it won't highlight correctly.
+
+The effort required to correct these (very minor) issues would be untenable, though. I can't see myself putting in the effort.
+
+## Future Plans
+
+There are a couple features that I'd still plan on adding:
+
+* Highlighting stack notation comments (e.g. `\ n1 n2 -- n3`)
+* Identify comments used for word definition help and highlight them
+* Run script in an external shell as opposed to the embedded terminal
+* Somehow add user-defined immediate words (`i:`) as to-be-highlighted
+
+## Bugs and Feature Requests
+
+If you find something that you think is wrong or have a feature request, feel free to open up an issue on [GitHub][issues].
+
+Also, if you find this extension helpful and want to give a shout-out, a great place to do so would be on the [8th forums][forums]!
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of Yadid
+
+
+
+[vscode]: https://code.visualstudio.com/
+[8th]: https://8th-dev.com/
+[ron]: https://8th-dev.com/aboutus#ron
+[forums]: https://8th-dev.com/forum
+[issues]: https://github.com/massung/yadid/issues
---
+++ src/yadidMain.ts
@@ -0,0 +1,200 @@
+import * as vscode from 'vscode';
+import { existsSync } from 'fs';
+import { delimiter, join } from 'path';
+
+/* When last prompted for command line arguments, these are what were used. */
+let lastCmdLineArgs = '';
+
+/* Extension is being loaded, register commands, etc.
+ */
+export function activate(context: vscode.ExtensionContext) {
+	let config = vscode.workspace.getConfiguration('yadid');
+
+	// register commands
+	vscode.commands.registerCommand('yadid.run.file', yadidRunFile);
+	vscode.commands.registerCommand('yadid.run.fileWithArgs', yadidRunFileWithArgs);
+	vscode.commands.registerCommand('yadid.build.project', yadidBuildProject);
+	vscode.commands.registerCommand('yadid.build.projectWithGUI', yadidBuildProjectWithGUI);
+}
+
+/* Extension in unloading, do cleanup work here.
+ */
+export function deactivate() {
+	// TODO:
+}
+
+/* Find the 8th executable in the PATH, if it exists, run the file.
+ */
+export function yadidRunFile() {
+	let path = find8th();
+
+	if (!path) {
+		vscode.window.showErrorMessage(`No '8th' binary found in PATH`);
+	} else {
+		runFile();
+	}
+}
+
+/* Prompt the user for command line arguments and then run.
+ */
+export function yadidRunFileWithArgs() {
+	let path = find8th();
+
+	if (!path) {
+		vscode.window.showErrorMessage(`No '8th' binary found in PATH`);
+	} else {
+		let opts = {
+			prompt: 'Command line arguments:',
+			ignoreFocusOut: true,
+			value: lastCmdLineArgs,
+		};
+
+		// ask for input, then run
+		vscode.window
+			.showInputBox(opts)
+			.then(value => {
+				if (value) {
+					runFile(lastCmdLineArgs = value);
+				}
+			});
+	}
+}
+
+/* Find the build binary next to 8th, if it exists, build the project.
+ */
+export function yadidBuildProject(gui: boolean = false) {
+	let path = find8th();
+
+	if (!path) {
+		vscode.window.showErrorMessage(`No '8th' binary found in PATH`);
+	} else {
+		let build = findBuildBinary(path);
+
+		if (!build) {
+			vscode.window.showErrorMessage(`No 'build' binary found relative to ${path}`);
+		} else {
+			getCurrentWorkspaceFolder()
+				.then(folder => {
+					if (build && folder) {
+						buildProject(build, folder, gui ? '' : '-g');
+					} else {
+						vscode.window.showErrorMessage(`No workspace open`);
+					}
+				})
+		}
+	}
+}
+
+/* Launch the build GUI instead of just building via terminal.
+ */
+export function yadidBuildProjectWithGUI() {
+	yadidBuildProject(true)
+}
+
+/* Looks for the 8th executable in the path, returns the first one it finds.
+ */
+function find8th(): string | undefined {
+	let path = undefined;
+	let paths = [];
+	let app = '8th';
+
+	// on windows, the 8th executable has a file extension
+	if (process.platform == 'win32') {
+		app += '.exe';
+	}
+
+	// find the app executable in the path somewhere
+	if (process.env['PATH']) {
+		paths = process.env['PATH']
+			.split(delimiter)
+			.map(path => join(path, app));
+
+		// return the first path that has an 8th binary
+		path = paths.find(existsSync);
+	}
+
+	return path;
+}
+
+/* Looks for the build binary relative to the 8th executable.
+ */
+function findBuildBinary(path: string | undefined): string | undefined {
+	if (path) {
+		let build = join(path, '../../build');
+
+		if (existsSync(build)) {
+			return build;
+		}
+	}
+
+	return undefined;
+}
+
+/* Determines what the "current" workspace folder is.
+ */
+function getCurrentWorkspaceFolder(): Thenable<vscode.WorkspaceFolder | undefined> {
+	let folders = vscode.workspace.workspaceFolders;
+	let editor = vscode.window.activeTextEditor;
+
+	// must have an open workspace
+	if (!folders || folders.length == 0) {
+		return Promise.resolve(undefined);
+	}
+
+	// if there's an active file being edited, return that file's workspace
+	if (editor) {
+		if (editor.document.isDirty) {
+			return editor.document
+				.save()
+				.then(success => {
+					if (editor && success) {
+						return vscode.workspace.getWorkspaceFolder(editor.document.uri);
+					}
+				});
+		} else {
+			return Promise.resolve(vscode.workspace.getWorkspaceFolder(editor.document.uri))
+		}
+	}
+
+	// if there's only 1 workspace, use it
+	if (folders.length == 1) {
+		return Promise.resolve(folders[0]);
+	}
+
+	// let the user pick the workspace
+	return vscode.window.showWorkspaceFolderPick({});
+}
+
+/* Executes the current file in the terminal.
+ */
+function runFile(args: string = '') {
+	let editor = vscode.window.activeTextEditor;
+	let terminal = vscode.window.createTerminal('8th');
+
+	if (editor && editor.document.isDirty) {
+		editor.document
+			.save()
+			.then(success => {
+				if (terminal && editor && success) {
+					terminal.show(true);
+					terminal.sendText(`8th ${editor.document.fileName} ${args}`, true);
+				}
+			});
+	} else {
+		if (terminal && editor) {
+			terminal.show(true);
+			terminal.sendText(`8th "${editor.document.fileName}" ${args}`, true);
+		}
+	}
+}
+
+/* Executes the build project command in the terminal.
+ */
+function buildProject(buildBinary: string, folder: vscode.WorkspaceFolder, flags: string) {
+	let terminal = vscode.window.createTerminal('8th');
+
+	if (terminal) {
+		terminal.show();
+		terminal.sendText(`8th ${buildBinary} ${flags} "${folder.uri.fsPath}"`, true);
+	}
+}
